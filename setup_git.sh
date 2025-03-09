c9f9d2d096891c849a8b2f672209409ea4afbe24
abeaufays <beaufays.alexandre@gmail.com>
2025-03-09 23:52:35 +0100

    Make setup_git.sh work outside of the repository

--- setup_git.sh
+++ setup_git.sh
@@ -1,14 +1,17 @@
-if [ $(git config --get-all include.path | grep $(pwd)/.gitconfig) ]; then
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+if [ $(git config --get-all include.path | grep $SCRIPT_DIR/.gitconfig) ]; then
     echo Link to this .gitconfig already exists
 else
-    git config --global --add include.path $(pwd)/.gitconfig
+    git config --global --add include.path $SCRIPT_DIR/.gitconfig
     echo This .gitconfig has been linked to global
 fi
 
-if [ $(git config --get-all core.excludesFile | grep $(pwd)/.gitignore.global) ]; then
+if [ $(git config --get-all core.excludesFile | grep $SCRIPT_DIR/.gitignore.global) ]; then
     echo Link to this .gitignore already exists
 else
-    git config --global --add core.excludesFile $(pwd)/.gitignore.global
+    git config --global --add core.excludesFile $SCRIPT_DIR/.gitignore.global
     echo This .gitignore has been linked to global
 fi
 
