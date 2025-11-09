f529b246e3ec35f00cfc69066c1fe3586b8e85c8
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2025-11-09 21:41:34 +0100

    Make setup_zsh.sh add scripts/bin to PATH

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -2,8 +2,25 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if ! grep -q "source $SCRIPT_DIR/zsh/prompt" ~/.zshrc; then
     echo "source $SCRIPT_DIR/zsh/prompt"  >> ~/.zshrc
+else
+    echo "zsh/prompt already in ~/.zshrc"
 fi
 
 if ! grep -q "source $SCRIPT_DIR/zsh/alias" ~/.zshrc; then
     echo "source $SCRIPT_DIR/zsh/alias"  >> ~/.zshrc
+else
+    echo "zsh/alias already in ~/.zshrc"
+fi
+
+
+if ! grep -q "export PATH=$SCRIPT_DIR/scripts/bin:\$PATH" ~/.zshrc; then
+    read -p "Add scripts/bin to PATH ? (Y/N): " confirm 
+    if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+        echo "export PATH=$SCRIPT_DIR/scripts/bin:\$PATH" >> ~/.zshrc
+    else
+        echo "Skipped"
+    fi
+
+else
+    echo "scripts/bin already added to PATH in ~/.zshrc"
 fi
